643fb1fb7864db276687a46c0d162f33720e63d4
wairimu.Kanene <mercykanene254@gmail.com>
2022-03-06 23:17:22 +0300

    commit9-called a method

---
+++ Akan.js
@@ -0,0 +1,4 @@
+let CC, YY, MM, DD, d, dayValue;
+let dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+let femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
