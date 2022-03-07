daac85134e8e229b64b5573e3528beb80e3e5875
wairimu.Kanene <mercykanene254@gmail.com>
2022-03-07 05:54:48 +0300

    commit14-functions

--- Akan.js
+++ Akan.js
@@ -25,4 +25,24 @@ function validate() {
         alert("Select either male or female");
         return false;
     }
+    else{
+        return true;
+    }
+    function calculateDayvalue(){
+        year= document.getElementById("year"). value;
+        CC=parseInt(year.substring(0,2));
+        YY=parseInt(year.substring(2,4));
+        MM=parseInt(document.getElementById("month").value);
+        DD=parseInt(document.getElementById("date").value);
+        d=(((CC/4) -2*CC-1) +( (5*YY/4) )+((26*(MM+1)/10))+DD)%7;
+        console.log(d);
+        return (Math.floor(d));
+    }
+    function getGender(){
+        var gender=document.getElementsByName("gender");
+        if(gender[0].checked==true){
+            var gender="male";
+        }
+        else if
+    }
 }
