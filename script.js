905b9779e8a5b69d69aa9cda7f7039b99d33282e
wairimu.Kanene <mercykanene254@gmail.com>
2022-03-11 11:30:40 +0300

    commit34-modified file form ,changed background image,changed js functionality

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 let genderPicked = document.querySelector("#gender");
 let akanName = document.getElementById('result');
 
-let dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-let maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+let dayNames=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let maleNames=["Kwasi,","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 let femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-
 function generateAkanName() {
     let pickedDate = document.querySelector("#date").value;
     let day = new Date(pickedDate).getDay();
-    
-    for (let gender of genderPicked) {
-    
-        let pickedGender = gender.value;
-        if (pickedGender === "male") {
-        akanName.innerText = `Your Akan name is ${maleNames[day]}`
-        ;
-        } else if (pickedGender === "female") {
+
+    //Checking for empty input fields
+    if(pickedDate == ""){
+        alert("Enter your Date of Birth!");
+    }else if(genderPicked.value === ""){
+        alert("Select your gender!");
+    }
+
+    //Checking the gender that was selected
+    if (genderPicked.value === "male") {
+        akanName.innerText = `Your Akan name is ${maleNames[day]}`;
+        return;
+    } 
+    if (genderPicked.value === "female") {
         akanName.innerText = `Your Akan name is ${femaleNames[day]}`;
-        }
+        return;
     }
 }
 
